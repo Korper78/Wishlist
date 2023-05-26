f7ffdd2a214dc22446d03d0d7c80c1cfe63ac530
Korper78 <korper78@gmail.com>
2023-05-26 13:51:40 +0300

    Day 3: modal

--- scripts/renderNavigation.js
+++ scripts/renderNavigation.js
@@ -1,5 +1,7 @@
 import { createBurgerMenu } from "./createBurgerMenu.js";
 import { createElement } from "./helper.js";
+import { API_URL } from "./const.js";
+import { renderModal } from "./renderModal.js";
 
 const nav = document.querySelector('.nav');
 createBurgerMenu(nav, 'nav_active');
@@ -12,8 +14,36 @@ export const renderNavigation = () => {
     textContent: 'Зарегистрироваться'
   })
 
-  buttonSignUp.addEventListener('click',{
+  buttonSignUp.addEventListener('click',() => {
+    renderModal({
+      title: 'Регистрация',
+      description: 'Введите ваши данные для регистрации на сервисе WishList',
+      btnSubmit: 'Зарегистрироваться',
+      submitHandler: async () => {
+        const formData = new FormData(event.target);
+        const credentials = {
+          login: formData.get('login'),
+          password: formData.get('password'),
+        };
+        try {
+          const response = await fetch(`${API_URL}/register`, {
+            method: 'POST',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(credentials),
+          });
 
+          if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+          } else {
+            console.log(await response.json());
+            throw new Error('Invalid credentials');
+          };
+        } catch(error) {
+
+        }
+      }
+    });
   });
   
   const buttonLogin = createElement('button', {
@@ -21,7 +51,7 @@ export const renderNavigation = () => {
     textContent: 'Войти'
   })
 
-  buttonLogin.addEventListener('click',{
+  buttonLogin.addEventListener('click', () => {
 
   });
   
